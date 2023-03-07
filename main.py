f8507eb10d94d374d3ce9e922b08d96bcdaacffb
farizdp <email.farizdp@gmail.com>
2023-03-07 15:22:05 +0700

    initial file

---
+++ main.py
@@ -0,0 +1,44 @@
+import requests, telepot, os
+import prettytable as pt
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+load_dotenv()
+id_telegram = os.getenv('ID_TELEGRAM')
+token = os.getenv('TOKEN')
+bot = telepot.Bot(token)
+
+r = requests.get('https://www.jadwalsholat.org/adzan/monthly.php?id=310')
+if r.status_code == 200:
+    html = BeautifulSoup(r.content, "html.parser")
+    kota = html.select_one('option[selected]').string
+    bulan = html.h2.string
+    jadwal = html.find_all('tr', class_="table_highlight")
+    results = []
+    for row in jadwal:
+        data = row.find_all('td')
+        row_values = []
+        for d in data:
+            row_values.append(d.get_text(strip=True))
+        results.append(row_values)
+    table = pt.PrettyTable()
+    table.title = results[0][0] + ' ' + bulan
+    table.field_names = ['Waktu', 'Jam']
+    table.align['Waktu'] = 'l'
+    data = [
+        ('Imsyak', results[0][1]),
+        ('Shubuh', results[0][2]),
+        ('Terbit', results[0][3]),
+        ('Dhuha', results[0][4]),
+        ('Dzuhur', results[0][5]),
+        ('Ashr', results[0][6]),
+        ('Maghrib', results[0][7]),
+        ('Isya', results[0][8]),
+    ]
+    for waktu, jam in data:
+        table.add_row([waktu, jam])
+    text = "Jadwal sholat *{0}*\n\n```{1}```".format(kota, table)
+else:
+    text = "Not found!"
+
+bot.sendMessage(id_telegram, text, 'Markdown')
